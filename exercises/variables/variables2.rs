88a59754909f7f2599c23cee1ccaeb012ffaf13e
hanser2678 <148211469+hanser2678@users.noreply.github.com>
2023-10-23 13:35:16 +0000

    update

--- exercises/variables/variables2.rs
+++ exercises/variables/variables2.rs
@@ -3,9 +3,9 @@
 // Execute `rustlings hint variables2` or use the `hint` watch subcommand for a
 // hint.
 
-
 fn main() {
-    let x: i32;// warning
+    let x: i32; // warning
+    x = 10;
     if x == 10 {
         println!("x is ten!");
     } else {
