006d28e16e080596cd63db6e08603c64de0337c8
hanser2678 <148211469+hanser2678@users.noreply.github.com>
2023-10-25 09:56:27 +0000

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,16 +3,16 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-
+use std::fmt;
 fn trim_me(input: &str) -> &str {
     // TODO: Remove whitespace from both ends of a string!
     input.trim()
 }
 
-// fn compose_me(input: &str) -> String {
-//     // TODO: Add " world!" to the string! There's multiple ways to do this!
-//     ???
-// }
+fn compose_me(input: &str) -> String {
+    // TODO: Add " world!" to the string! There's multiple ways to do this!
+    return  fmt::format(format_args!("{} world!", input));
+}
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
@@ -30,11 +30,11 @@ mod tests {
         assert_eq!(trim_me("   Hola!  "), "Hola!");
     }
 
-    // #[test]
-    // fn compose_a_string() {
-    //     assert_eq!(compose_me("Hello"), "Hello world!");
-    //     assert_eq!(compose_me("Goodbye"), "Goodbye world!");
-    // }
+    #[test]
+    fn compose_a_string() {
+        assert_eq!(compose_me("Hello"), "Hello world!");
+        assert_eq!(compose_me("Goodbye"), "Goodbye world!");
+    }
 
     #[test]
     fn replace_a_string() {
